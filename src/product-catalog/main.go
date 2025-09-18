9b728cd9bde6a34476a98e84ecef9e1c535e6fe3
eugenekofi <eugenepomevor@gmail.com>
2025-09-18 20:21:38 +0000

    unused createClient function & deprecated api removed

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -8,7 +8,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io/fs"
 	"net"
 	"os"
@@ -145,7 +144,7 @@ func main() {
 
 	log.Infof("Product Catalog gRPC server started on port: %s", port)
 
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	ln, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatalf("TCP Listen: %v", err)
 	}
@@ -179,8 +178,6 @@ type productCatalog struct {
 }
 
 func readProductFiles() ([]*pb.Product, error) {
-
-	// find all .json files in the products directory
 	entries, err := os.ReadDir("./products")
 	if err != nil {
 		return nil, err
@@ -197,8 +194,6 @@ func readProductFiles() ([]*pb.Product, error) {
 		}
 	}
 
-	// read the contents of each .json file and unmarshal into a ListProductsResponse
-	// then append the products to the catalog
 	var products []*pb.Product
 	for _, f := range jsonFiles {
 		jsonData, err := os.ReadFile("./products/" + f.Name())
@@ -215,7 +210,6 @@ func readProductFiles() ([]*pb.Product, error) {
 	}
 
 	log.Infof("Loaded %d products", len(products))
-
 	return products, nil
 }
 
@@ -250,9 +244,8 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 		attribute.String("app.product.id", req.Id),
 	)
 
-	// GetProduct will fail on a specific product when feature flag is enabled
 	if p.checkProductFailure(ctx, req.Id) {
-		msg := fmt.Sprintf("Error: Product Catalog Fail Feature Flag Enabled")
+		msg := "Error: Product Catalog Fail Feature Flag Enabled"
 		span.SetStatus(otelcodes.Error, msg)
 		span.AddEvent(msg)
 		return nil, status.Errorf(codes.Internal, msg)
@@ -267,13 +260,13 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 	}
 
 	if found == nil {
-		msg := fmt.Sprintf("Product Not Found: %s", req.Id)
+		msg := "Product Not Found: " + req.Id
 		span.SetStatus(otelcodes.Error, msg)
 		span.AddEvent(msg)
 		return nil, status.Errorf(codes.NotFound, msg)
 	}
 
-	msg := fmt.Sprintf("Product Found - ID: %s, Name: %s", req.Id, found.Name)
+	msg := "Product Found - ID: " + req.Id + ", Name: " + found.Name
 	span.AddEvent(msg)
 	span.SetAttributes(
 		attribute.String("app.product.name", found.Name),
@@ -308,33 +301,3 @@ func (p *productCatalog) checkProductFailure(ctx context.Context, id string) boo
 	)
 	return failureEnabled
 }
-
-func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, svcAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
-	)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
